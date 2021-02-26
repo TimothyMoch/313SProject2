b529e73f19dce9cd7a7e86c5510db4e7192f3b92
Scott Carrion <scott.carrion@linux2.cs.tamu.edu>
2021-02-26 17:46:22 -0600

    Checked in .sql scripts (one example, one for our testcases) for phase 2

---
+++ queries.sql
@@ -0,0 +1,11 @@
+-- SQL file for phase 2 database correctness demonstration
+-- CSCE 315 Project 2, Group 11, Spring 2021
+-- We verify that this code was jointly written in adherence to the Aggie Code of Honor
+--
+-- Copyright notice: This code is for academic use only. No license or warranty is provided.
+-- Invoke this from the psql command line using \i <path_to_sql_file>
+
+-- Here are 20 test case queries that demonstrate the correctness of our database!
+SELECT * FROM orders WHERE menuitem="E5";
+
+-- TODO: WRITE MORE QUERIES LOL
---
+++ test_query.sql
@@ -0,0 +1,3 @@
+-- Example SQL file.
+-- Invoke this from the psql command line using \i <path_to_sql_file>
+SELECT orderid, FROM orders WHERE menuitem="E5";  -- Query 1
