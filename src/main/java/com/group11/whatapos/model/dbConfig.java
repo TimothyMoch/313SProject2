c77a816479050ae05ff2e6d36275b7289a2754e5
Ryan O'Malley <rd.omalley@comcast.net>
2021-03-08 16:25:57 -0600

    Create dbConfig.java

---
+++ src/main/java/com/group11/whatapos/model/dbConfig.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.group11.whatapos.model;
+
+/**
+ *
+ * @author ryanomalley
+ */
+public class dbConfig {
+    public static final String user = "";
+    public static final String pswd = "";
+    public static final String url = "jdbc:postgresql://csce-315-db.engr.tamu.edu/db904_group11_project2";
+    
+}
