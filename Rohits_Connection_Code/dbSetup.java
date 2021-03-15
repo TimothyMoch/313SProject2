c052c19cc08bd0e0fbf1d96b9cc3c37385725244
Rohit Barichello <rohitbarichello@gmail.com>
2021-03-15 00:53:52 -0500

    removed rohits connection code folder

--- Rohits_Connection_Code/dbSetup.java
+++
@@ -1,9 +0,0 @@
-/*
-CSCE 315
-Database setup example
-Purpose: Keeping usernames and password private.
-*/
-public final class dbSetup  {
-  public static final String user = "rohitbarichello";
-  public static final String pswd = "627006807";
-}//end class
